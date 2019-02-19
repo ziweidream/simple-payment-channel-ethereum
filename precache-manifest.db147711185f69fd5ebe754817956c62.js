f17f72f3f716617df5daa974ad8e55eeb11723fd
ziweidream <ziwei.lotus@gmail.com>
2019-02-19 09:27:20 -0500

    Updates

---
+++ precache-manifest.db147711185f69fd5ebe754817956c62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd8009f4cc50ae957d21",
+    "url": "/simple-payment-channel-ethereum/static/js/runtime~main.cd8009f4.js"
+  },
+  {
+    "revision": "be9b7de8ca49c3de8725",
+    "url": "/simple-payment-channel-ethereum/static/js/main.be9b7de8.chunk.js"
+  },
+  {
+    "revision": "2a1f3dec5a01955aa45f",
+    "url": "/simple-payment-channel-ethereum/static/js/2.2a1f3dec.chunk.js"
+  },
+  {
+    "revision": "be9b7de8ca49c3de8725",
+    "url": "/simple-payment-channel-ethereum/static/css/main.067cd7a2.chunk.css"
+  },
+  {
+    "revision": "2e8e2952e19a3f3c8e3c2711c451202d",
+    "url": "/simple-payment-channel-ethereum/index.html"
+  }
+];
